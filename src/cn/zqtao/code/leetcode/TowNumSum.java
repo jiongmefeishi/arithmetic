6b96536fb6434a422da50163b9dadfbd88e4266d
zqtao <453395742@qq.com>
2019-03-20 17:47:34 +0800

    两数之和

---
+++ src/cn/zqtao/code/leetcode/TowNumSum.java
@@ -0,0 +1,62 @@
+package cn.zqtao.code.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @auther: zqtao
+ * @description: 两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * @version: 1.0
+ */
+public class TowNumSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    /**
+     * 将元素存储在map集合中
+     * <key=元素值, value=元素下标2>
+     * 搜索数组，map中存在target - nums[i] 这个key值 ，则返回
+     * 不存在，则添加到map
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums.length == 0) return new int[0];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+            } else {
+                map.putIfAbsent(nums[i], i);
+            }
+        }
+        return res;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
