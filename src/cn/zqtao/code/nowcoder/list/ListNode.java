1d1349142fa3d7d3cb789c9a7bc4d59206b7aa2f
zqtao <453395742@qq.com>
2019-03-31 20:34:57 +0800

    单链表 - 反转链表

---
+++ src/cn/zqtao/code/nowcoder/list/ListNode.java
@@ -0,0 +1,38 @@
+package cn.zqtao.code.nowcoder.list;
+
+/**
+ * @auther: zqtao
+ * @description: 单链表
+ * @version: 1.0
+ */
+public class ListNode {
+
+    int val;
+    ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
+class Solution{
+
+    /**
+     * 反转链表
+     * 并返回头结点
+     */
+    public ListNode ReverseList(ListNode head){
+        if (head == null || head.next == null) return head;
+
+        ListNode pre = null;
+        ListNode next = null;
+
+        while (head.next != null){
+            next = head.next;
+            //pre始终指向当前head的前一个节点，这样可以反转节点了。
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
