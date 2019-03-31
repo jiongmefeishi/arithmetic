69d9541818749a42368d2a6fb6750a6bb2247a29
zqtao <453395742@qq.com>
2019-03-31 23:04:38 +0800

    Update ListNode.java

--- src/cn/zqtao/code/nowcoder/list/ListNode.java
+++ src/cn/zqtao/code/nowcoder/list/ListNode.java
@@ -26,7 +26,7 @@ class Solution{
         ListNode pre = null;
         ListNode next = null;
 
-        while (head.next != null){
+        while (head != null){
             next = head.next;
             //pre始终指向当前head的前一个节点，这样可以反转节点了。
             head.next = pre;
