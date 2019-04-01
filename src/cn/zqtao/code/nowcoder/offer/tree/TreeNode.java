aad61d7660bc8546b841e8e2517e245feeac94b5
zqtao <453395742@qq.com>
2019-04-01 16:18:06 +0800

    Create TreeNode.java

---
+++ src/cn/zqtao/code/nowcoder/offer/tree/TreeNode.java
@@ -0,0 +1,14 @@
+package cn.zqtao.code.nowcoder.offer.tree;
+
+/**
+ * Definition for binary tree
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
