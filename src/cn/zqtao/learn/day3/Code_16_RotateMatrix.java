ad0b7c7732c365664b1332b7e2a4f4f16a56223f
zqtao <453395742@qq.com>
2019-07-10 21:30:30 +0800

    Update Code_16_RotateMatrix.java

--- src/cn/zqtao/learn/day3/Code_16_RotateMatrix.java
+++ src/cn/zqtao/learn/day3/Code_16_RotateMatrix.java
@@ -3,6 +3,12 @@ package cn.zqtao.learn.day3;
 /**
  * @auther: zqtao
  * @description: 旋转正方形矩阵, 翻转90度
+ * 旋转正方形矩阵
+ *
+ * 【题目】 给定一个整型正方形矩阵matrix，
+ * 请把该矩阵调整成 顺时针旋转90度的样子。
+ *
+ * 【要求】 额外空间复杂度为O(1)。
  * @version: 1.0
  */
 public class Code_16_RotateMatrix{
