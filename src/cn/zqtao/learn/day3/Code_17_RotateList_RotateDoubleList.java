6d90feb9be09e2322bec65193470f0c136cc567d
zqtao <453395742@qq.com>
2019-07-10 21:30:41 +0800

    Update Code_17_RotateList_RotateDoubleList.java

--- src/cn/zqtao/learn/day3/Code_17_RotateList_RotateDoubleList.java
+++ src/cn/zqtao/learn/day3/Code_17_RotateList_RotateDoubleList.java
@@ -2,7 +2,11 @@ package cn.zqtao.learn.day3;
 
 /**
  * @auther: zqtao
- * @description:
+ * @description: 反转单向和双向链表
+ * 【题目】 分别实现反转单向链表和反转双向链表的函数。
+ * 【要求】 如果链表长度为N，
+ * 时间复杂度要求为O(N)，
+ * 额外空间 复杂度要求为O(1)
  * @version: 1.0
  */
 public class Code_17_RotateList_RotateDoubleList {
