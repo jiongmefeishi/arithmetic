6e8d82ec61fcd4203e6b25fce2c4f108d80485ac
zqtao <453395742@qq.com>
2019-07-20 18:17:21 +0800

    Create ComparatorModel.java

---
+++ src/cn/zqtao/learn/model/ComparatorModel.java
@@ -0,0 +1,34 @@
+package cn.zqtao.learn.model;
+
+import java.util.Comparator;
+
+/**
+ * @auther: zqtao
+ * @description: 比较器模型
+ * @version: 1.0
+ */
+public class ComparatorModel {
+    /**
+     * 小根堆比较器
+     */
+    public static class MinHeapComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1 - o2; // < 0  o1 < o2  负数
+        }
+    }
+
+    /**
+     * 大根堆比较器
+     */
+    public static class MaxheapComparator implements Comparator<Integer> {
+
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1; // <   o2 < o1
+        }
+
+    }
+
+}
