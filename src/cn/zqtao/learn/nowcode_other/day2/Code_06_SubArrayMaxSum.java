6b99539066e9f4b3061597ab76d9c322f7a3ed85
zqtao <453395742@qq.com>
2019-08-01 13:05:48 +0800

    Create Code_06_SubArrayMaxSum.java

---
+++ src/cn/zqtao/learn/nowcode_other/day2/Code_06_SubArrayMaxSum.java
@@ -0,0 +1,30 @@
+package cn.zqtao.learn.nowcode_other.day2;
+
+/**
+ * @auther: zqtao
+ * @description: 子数组的最大累加和
+ * 给定一个数组arr,返回子数组的最大累加和。
+ * 例如, arr=[1,-2, 3,5,-2,6,-1],所有的子数组中,
+ * [3, 5,-2,6]可以累加出最大的和12,所以返回12.
+ * @version: 1.0
+ */
+public class Code_06_SubArrayMaxSum {
+
+    public static int maxSubArr(int[] arr) {
+        if (arr == null || arr.length == 0)return 0;
+
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
+        for (int i = 0; i < arr.length; i++) {
+            cur += arr[i];
+            max = Math.max(max, cur);
+            cur = cur < 0 ? 0 : cur;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1,1,2,-8,12,1,-3,-4,6};
+        System.out.println(maxSubArr(arr));
+    }
+}
