c47925ae9cf4f1273b6b310f4d1e84d5658eaf88
zqtao <453395742@qq.com>
2019-08-28 07:25:49 +0800

    Create Code_36_.java

---
+++ src/cn/zqtao/learn/nowcode_other/day910/Code_36_.java
@@ -0,0 +1,21 @@
+package cn.zqtao.learn.nowcode_other.day910;
+
+/**
+ * @auther: zqtao
+ * @description:
+ *
+ *
+ * @version: 1.0
+ */
+public class Code_36_ {
+
+    /**
+     * KMP算法
+     * KMP算法 解决 str1 中是否包含 str2
+     * 包含返回 str2 开始位置，不包含返回 -1
+     *
+     * 1、暴力方法：以str1 的每一个字符去 匹配str2 的每一个字符，str1长N, str2长M,时间复杂度 O(M*N)
+     * 2、KMP算法 str1长N, str2长M 可以优化到 O(N) N > M
+     *
+     */
+}
