42cf4bbcef653eaa6e603f6d884c40edc9063f2c
zqtao <453395742@qq.com>
2019-09-03 09:15:38 +0800

    Update Code_42_LongestSubArrayLessSum.java

--- src/cn/zqtao/learn/nowcode_other/day11/Code_42_LongestSubArrayLessSum.java
+++ src/cn/zqtao/learn/nowcode_other/day11/Code_42_LongestSubArrayLessSum.java
@@ -9,11 +9,27 @@ import java.util.HashMap;
  */
 public class Code_42_LongestSubArrayLessSum {
 
+    /**
+     * 时间复杂度 O(N)
+     * 思路：
+     * 第一步：1、获取以i 位置开始 i~N-1 能够累加出的最小和
+     *        2、记录最小和累加的右边界
+     * 第二步：滑动窗口求取最大长度
+     * 其中的加速过程是：不需要每一个 i 位置都进行向后累加，可以借助前一次累加的最大结果 - (i-1)位置的结果
+     * 得到的就是 i~(i-1)所达到的最大值
+     *
+     * 如下面范围内都是第一步得到的最小累加和的区域
+     * 0~6  7~9   10~17   18~23   24~28
+     *
+     * 0位置开始窗口扩容，直到 10~17 位置内累加和都是小于 K 的， 累加18~23区域会大于 K，那么0~17范围累加和为 sum
+     * 此时开始从 1 位置遍历，不需要从新从 1~6 .... 10~17 依次累加，只需要 sum-arr[0] 即可得到新 sum 这就是加速过程
+     *
+     */
     public static int maxLengthSumLessK(int[] arr, int K) {
         if (arr == null || arr.length == 0) return 0;
 
         int[] sums = new int[arr.length]; // 以i 位置开始 i~N-1 能够累加出的最小和
-        HashMap<Integer, Integer> ends = new HashMap<>(); // 记录累加最小和的右边界
+        HashMap<Integer, Integer> ends = new HashMap<>(); // 记录最小累加和的右边界
 
         sums[arr.length - 1] = arr[arr.length - 1];// 最后一个数累加最小和就是自己
         ends.put(arr.length - 1, arr.length - 1);
