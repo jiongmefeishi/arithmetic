66138b13f5ff78b7934183cd20e9579b48cb8cf9
zqtao <453395742@qq.com>
2019-09-11 10:08:01 +0800

    Create Node.java

---
+++ src/cn/zqtao/learn/zcy_code/tree/model/Node.java
@@ -0,0 +1,17 @@
+package cn.zqtao.learn.zcy_code.tree.model;
+
+/**
+ * @auther: zqtao
+ * @description: 二叉树节点模型
+ * @version: 1.0
+ */
+public class Node {
+
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
