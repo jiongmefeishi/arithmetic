d1c2e3552c568a5703e1918f98ffb886cb0506a3
zqtao <453395742@qq.com>
2019-09-11 10:08:15 +0800

    Create PrintBT.java

---
+++ src/cn/zqtao/learn/zcy_code/tree/model/PrintBT.java
@@ -0,0 +1,37 @@
+package cn.zqtao.learn.zcy_code.tree.model;
+
+/**
+ * @auther: zqtao
+ * @description: 直观打印3二叉树
+ * @version: 1.0
+ */
+public class PrintBT {
+    public static void printTree(Node head) {
+        System.out.println("Binary Tree:");
+        printInOrder(head, 0, "H", 17);
+        System.out.println();
+    }
+
+    private static void printInOrder(Node head, int height, String to, int len) {
+        if (head == null) {
+            return;
+        }
+        printInOrder(head.right, height + 1, "v", len);
+        String val = to + head.value + to;
+        int lenM = val.length();
+        int lenL = (len - lenM) / 2;
+        int lenR = len - lenM - lenL;
+        val = getSpace(lenL) + val + getSpace(lenR);
+        System.out.println(getSpace(height * len) + val);
+        printInOrder(head.left, height + 1, "^", len);
+    }
+
+    private static String getSpace(int num) {
+        String space = " ";
+        StringBuffer buf = new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
+    }
+}
