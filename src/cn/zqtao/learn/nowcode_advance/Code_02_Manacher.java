e36fc91fce3af8bc4f343ca5fd2e3a45c7ba5864
zqtao <453395742@qq.com>
2019-11-03 23:53:49 +0800

    Update Code_02_Manacher.java

--- src/cn/zqtao/learn/nowcode_advance/Code_02_Manacher.java
+++ src/cn/zqtao/learn/nowcode_advance/Code_02_Manacher.java
@@ -2,9 +2,13 @@ package cn.zqtao.learn.nowcode_advance;
 
 /**
  * @auther: zqtao
- * @description: 解决问题：回文
+ * @description: 解决问题：回文问题
  * @version: 1.0
  */
 public class Code_02_Manacher {
+
+    public static void method(){
+
+    }
 
 }
