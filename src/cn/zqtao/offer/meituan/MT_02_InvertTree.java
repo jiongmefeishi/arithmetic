384c6ff3f776818716805c956f5f496e816198fa
zqtao <453395742@qq.com>
2020-03-26 17:00:40 +0800

    Create MT_02_InvertTree.java

---
+++ src/cn/zqtao/offer/meituan/MT_02_InvertTree.java
@@ -0,0 +1,68 @@
+package cn.zqtao.offer.meituan;
+
+import java.util.LinkedList;
+
+/**
+ * @auther: zqtao
+ * @description: 翻转二叉树
+ * @version: 1.0
+ */
+public class MT_02_InvertTree {
+
+    // 递归 递归实现也就是深度优先遍历的方式
+    public TreeNode invertTree(TreeNode root) {
+        //递归函数的终止条件，节点为空时返回
+        if (root == null) {
+            return null;
+        }
+        //下面三句是将当前节点的左右子树交换
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = tmp;
+        //递归交换当前节点的 左子树
+        invertTree(root.left);
+        //递归交换当前节点的 右子树
+        invertTree(root.right);
+        //函数返回时就表示当前这个节点，以及它的左右子树
+        //都已经交换完了
+        return root;
+    }
+
+    // 迭代就是广度优先遍历，需要额外的数据结构：队列
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        //将二叉树中的节点逐层放入队列中，再迭代处理队列中的元素
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            //每次都从队列中拿一个节点，并交换这个节点的左右子树
+            TreeNode tmp = queue.poll();
+            TreeNode left = tmp.left;
+            tmp.left = tmp.right;
+            tmp.right = left;
+            //如果当前节点的左子树不为空，则放入队列等待后续处理
+            if (tmp.left != null) {
+                queue.add(tmp.left);
+            }
+            //如果当前节点的右子树不为空，则放入队列等待后续处理
+            if (tmp.right != null) {
+                queue.add(tmp.right);
+            }
+
+        }
+        //返回处理完的根节点
+        return root;
+    }
+
+    class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
