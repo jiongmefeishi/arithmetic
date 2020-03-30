067569aa2fa8d69869720b725e01b77d6d73eba4
zqtao <453395742@qq.com>
2020-03-30 22:28:28 +0800

    Create MT_04_Gupiao5.java

---
+++ src/cn/zqtao/offer/meituan/MT_04_Gupiao5.java
@@ -0,0 +1,48 @@
+package cn.zqtao.offer.meituan;
+
+import static java.lang.Math.max;
+
+/**
+ * @auther: zqtao
+ * @description: k = 2
+ *
+ * 一次交易由买入和卖出构成，至少需要两天。所以说有效的限制 k 应该不超过 n/2，
+ * 如果超过，就没有约束作用了，相当于 k = +infinity。这种情况是之前解决过的。
+ *
+ * @version: 1.0
+ */
+public class MT_04_Gupiao5 {
+
+    int maxProfit_k_any(int max_k, int[] prices) {
+        int n = prices.length;
+        if (max_k > n / 2)
+            return maxProfit_k_inf(prices);
+
+        int[][][] dp = new int[n][max_k + 1][2];
+        for (int i = 0; i < n; i++)
+            for (int k = max_k; k >= 1; k--) {
+                if (i - 1 == -1) {
+                    /* 处理 base case */
+                    dp[i][k][0] = 0;
+                    dp[i][k][1] = -prices[i];
+                    continue;
+                }
+                dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
+                dp[i][k][1] = max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+            }
+        return dp[n - 1][max_k][0];
+    }
+
+
+    // 第二题
+    int maxProfit_k_inf(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
+    }
+}
